55ec4064553687fbc0f90e26ef339265cc4ea992
sabilillah <sabilillah272@gmail.com>
2024-08-24 18:55:54 +0700

    Add delete cart item handler

--- src/routes/cartItems.ts
+++ src/routes/cartItems.ts
@@ -13,32 +13,31 @@ cartItemsRoutes.put(
   zValidator("json", upsertCartItemValidation, async (result, c) => {
     if (!result.success) throw new HTTPException(400, { message: "Validation error.", cause: parseError(result.error) });
 
-    const { cartId, productSlug, quantity } = result.data;
-    const productCount = await prisma.product.count({ where: { slug: productSlug } });
-
-    if (productCount !== 1) throw new HTTPException(404, { message: "Product not found." });
+    const { productSlug, quantity } = result.data;
+    const user = c.get("user");
 
     const item = await prisma.cartItem
       .upsert({
-        update: { cart: { connect: { id: cartId } }, product: { connect: { slug: productSlug } }, quantity },
-        create: { cart: { connect: { id: cartId } }, product: { connect: { slug: productSlug } }, quantity },
-        where: { item: { productSlug, cartId } },
+        update: { cart: { connect: { id: user.cart!.id } }, product: { connect: { slug: productSlug } }, quantity },
+        create: { cart: { connect: { id: user.cart!.id } }, product: { connect: { slug: productSlug } }, quantity },
+        where: { item: { productSlug, cartId: user.cart!.id } },
       })
       .then(async (result) => {
-        const items = await prisma.cartItem.findMany({ where: { cartId }, include: { product: true } });
+        const items = await prisma.cartItem.findMany({ where: { cartId: user.cart!.id }, include: { product: true } });
         const totalPrice = calculateTotalPrice(items);
 
         await prisma.cart.update({
-          where: { id: cartId },
+          where: { id: user.cart!.id },
           data: {
             totalPrice,
           },
         });
 
         return result;
+      })
+      .catch(() => {
+        throw new HTTPException(404, { message: "Product not found." });
       });
-
-    console.log(item);
 
     return c.json({ status: "success", code: 200, data: { item } }, 200);
   }),
@@ -53,9 +52,38 @@ cartItemsRoutes.get(
 
     const item = await prisma.cartItem.findUnique({ where: { item: { cartId: user.cart!.id, productSlug } }, include: { product: true } });
 
+    if (!item) throw new HTTPException(404, { message: "Product not found." });
+
     return c.json({ status: "success", code: 200, data: { item } }, 200);
   }),
 );
-cartItemsRoutes.delete("/:productSlug", (c) => c.text(c.req.param("productSlug")));
+cartItemsRoutes.delete(
+  "/:productSlug",
+  zValidator("param", findItemValidation, async (result, c) => {
+    if (!result.success) throw new HTTPException(400, { message: "Validation error.", cause: parseError(result.error) });
+
+    const { productSlug } = result.data;
+    const user = c.get("user");
+
+    await prisma.cartItem
+      .delete({ where: { item: { cartId: user.cart!.id, productSlug } } })
+      .then(async () => {
+        const items = await prisma.cartItem.findMany({ where: { cartId: user.cart!.id }, include: { product: true } });
+        const totalPrice = calculateTotalPrice(items);
+
+        await prisma.cart.update({
+          where: { id: user.cart!.id },
+          data: {
+            totalPrice,
+          },
+        });
+      })
+      .catch(() => {
+        throw new HTTPException(404, { message: "Product not found." });
+      });
+
+    return c.json({ status: "success", code: 200, data: { message: "Item deleted." } }, 200);
+  }),
+);
 
 export default cartItemsRoutes;
